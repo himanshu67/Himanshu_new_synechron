4acf16b180cf84e4fbe5e0b2a3a79d350a1f9ff1
Himanshu Gupta <Gupta.Himanshu@synechron.com>
2017-07-03 18:33:57 +0530

    my first commit"

---
+++ src/com/servlet/AppConfig.java
@@ -0,0 +1,33 @@
+package com.servlet;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.annotation.HandlesTypes;
+
+import org.apache.coyote.http11.Http11AprProtocol;
+
+@HandlesTypes({
+    javax.servlet.http.HttpServlet.class,
+    javax.servlet.Filter.class
+})
+public class AppConfig implements ServletContainerInitializer {
+	@Override
+	public void onStartup(Set<Class<?>> arg0, ServletContext appContext)
+			throws ServletException {
+
+		System.out.println(" in startup himanshu-->>>");
+		for (Iterator iterator = arg0.iterator(); iterator.hasNext();) {
+			Class<?> class1 = (Class<?>) iterator.next();
+		}
+		ServletRegistration.Dynamic dispatcher = appContext.addServlet(
+				"dispatcher", new HelloServlet());
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+  }
+
+}
